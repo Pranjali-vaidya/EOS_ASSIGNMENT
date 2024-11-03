30292ae93f33628dbcb27f22be1098fce1507644
Pranjali-vaidya <vaidyapranjali9@gmail.com>
2024-11-03 23:21:37 +0530

    Add files via upload

---
+++ assign5que1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo -e -n "Login Name : "
+whoami
+
+echo -n "Login name : "
+echo $LOGNAME
+
+echo "Current Dir : "
+pwd
+
+echo -e -n "Current Shell : "
+echo "$SHELL"
+
+echo -n "User : "
+echo $USER
+
+echo -n "Operating system : "
+uname
+
+echo "Home Directory : $HOME"
+
+
